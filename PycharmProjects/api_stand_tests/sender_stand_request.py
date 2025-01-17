d2d2a691da25a3e34108caa821c6abc091b07195
Alejandra <aleejaalvarez@gmail.com>
2025-01-16 21:17:02 -0700

    Mi primer commit

---
+++ PycharmProjects/api_stand_tests/sender_stand_request.py
@@ -0,0 +1,32 @@
+import configuration
+import requests
+import data
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+print(response.json())
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+response = get_users_table()
+print(response.status_code)
+
+def negative_assert_no_firstname(user_body):
+    response = sender_stand_request.post_new_user(user_body)
+
+    assert response.status_code == 400
+    assert response.json()["code"] == 400
+    assert response.json()["message"] == "No se enviaron todos los parámetros requeridos"
+
+def test_create_user_no_first_name_get_error_response():
+    user_body = data.user_body.copy()
+    user_body.pop("firstName")
+    negative_assert_no_firstname(user_body)
+
+
